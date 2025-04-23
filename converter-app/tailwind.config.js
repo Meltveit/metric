22758552428a6364ed9d2894cac6a99301c9cf58
Meltveit <meltveit00@gmail.com>
2025-04-23 13:37:34 +0200

    1

---
+++ converter-app/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './**/*.html',
+    './js/**/*.js',
+  ],
+  darkMode: false, // eller 'media' eller 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
